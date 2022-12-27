8a4abb43a5b3a6cc9de98f4adf109ef65c246b84
RDesign-Castex <113470690+RDesign-Castex@users.noreply.github.com>
2022-12-27 15:08:54 -0300

    Add files via upload

---
+++ src/js/contacto.js
@@ -0,0 +1,16 @@
+const $form = document.querySelector('#form')
+const $buttonMailto = document.querySelector('#trucazo')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  $buttonMailto.setAttribute(
+    'href',
+    `mailto:designrdcastex@gmail.com.com?subject=nombre ${form.get(
+      'name',
+    )}  correo ${form.get('email')}&body=${form.get('message')}`,
+  )
+  $buttonMailto.click
+    }
